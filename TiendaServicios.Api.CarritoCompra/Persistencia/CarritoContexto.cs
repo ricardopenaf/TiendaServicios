3f20c3db9804d5831a675b5586cee0cf307b81bf
Ricardo Pena Forero <ricardopenaf@gmail.com>
2026-09-11 17:01:13 -0500

    Carrito compras

---
+++ TiendaServicios.Api.CarritoCompra/Persistencia/CarritoContexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaServicios.Api.CarritoCompra.Modelo;
+
+namespace TiendaServicios.Api.CarritoCompra.Persistencia
+{
+    public class CarritoContexto : DbContext
+    {
+        public CarritoContexto(DbContextOptions<CarritoContexto> options) : base(options) { }
+
+        public DbSet<CarritoSesion> CarritoSesion { get; set; }
+        public DbSet<CarritoSesionDetalle> CarritoSesionDetalle { get; set; }
+
+    }
+}
---
+++ TiendaServicios.Api.CarritoCompra/Program.cs
@@ -0,0 +1,52 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation();
+//builder.Services.AddValidatorsFromAssemblyContaining<Nuevo>();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TiendaServicios.Api.CarritoCompra",
+        Version = "v1",
+        Description = "API del microservicio de carrito de compras"
+    });
+});
+
+//builder.Services.AddDbContext<ContextoLibreria>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionDatabase"));
+//});
+
+//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Nuevo.Manejador).Assembly));
+//builder.Services.AddAutoMapper(cfg => cfg.AddMaps(typeof(Nuevo).Assembly));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TiendaServicios.Api.CarritoCompra v1");
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
